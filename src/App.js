b806d8214a3456dc11e38f557cf0334f309c756b
Valerie Vazquez <vnvazquez68@gmail.com>
2020-04-28 19:59:32 -0600

    Content Shows On Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React ,{ useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+// import Info from "./Info";
+
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
-}
 
+  const [nasaPic, setNasaPic] = useState('');
+  const [title, setTitle] = useState('Title');
+  const [date, setDate] = useState('Date');
+  const [details, setDetails] = useState('Explanation')
+
+  useEffect(() => {
+    axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=mmL0vaLzouHjtF71JX0nDc4AFNRnJ1eWCvUIj7oR")
+
+    .then((res) =>{
+      setNasaPic(res.data.url);
+      setTitle(res.data.title);
+      setDate(res.data.date);
+      setDetails(res.data.explanation);
+    })
+
+    .catch(err => console.log(`There was an error: `, err));
+}, []);
+
+  return (
+    
+    <div className="App">
+      <h1>NASA Photo Of The Day</h1>
+      <h2>{title}</h2>
+      <h3>{date}</h3>
+      <img src={nasaPic} alt="space" />
+      <p>{details}</p>
+    </div>
+
+  );
+
+    
+}
 export default App;
